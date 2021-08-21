17c873659a33a8e48a5ff40de047eda02e736efa
Preston Davis <prem.dav@outlook.com>
2021-08-20 21:31:45 -0500

    trying to figure out how to deal with case when two numbers add to over 10..

--- leetcode/add-two-numbers.js
+++ leetcode/add-two-numbers.js
@@ -40,18 +40,71 @@ const constructList = (val, next) => {
 const addTwoNumbers = (l1, l2) => {
     const list1 = cycleList(l1, []);
     const list2 = cycleList(l2, []);
-    let list1_num = '0', list2_num = '0', sum = 0;
-    list1.reverse().forEach((elem) => {
+    let list1_num = '0', list2_num = '0';
+    list1.forEach((elem) => {
         list1_num = `${list1_num}${elem.val.toString()}`;
     });
+    console.log(list1_num)
     
-    list2.reverse().forEach((elem) => {
+    list2.forEach((elem) => {
         list2_num = `${list2_num}${elem.val.toString()}`;
     });
-    sum = parseInt(list1_num.substring(1), 10) + parseInt(list2_num.substring(1), 10);
-    sum = sum.toString().split('');
-    const nums = sum.reverse().map((item) => (item = parseInt(item, 10)));
+    console.log(list2_num)
+    let total = [];
+    let lastOver10 = false;
+    if (list1_num.length > list2_num.length){
+        for (let index = 0; index < list1_num.length; index++) {
+            if (list2_num[index] !== undefined && list2_num[index] !== null) {
+               let temp2 = (parseInt(list1_num[index], 10) + parseInt(list2_num[index], 10));
+                if (temp2 < 10) {
+                    total.push(temp2.toString());
+                    if (lastOver10) {
+                        lastOver10 = false;
+                        total[index] = (parseInt(total[index]) + 1).toString();
+                    }
+                }
+                else {
+                    if (lastOver10) {
+                        lastOver10 = false;
+                        total.push('1')
+                    }
+                    lastOver10 = true;
+                    total.push(temp2.toString().substring(1));
+                }
+            } else {
+                total.push(list1_num[index]);
+            }
+        }
+    } else {
+        for (let index = 0; index < list2_num.length; index++) {
+            if (list1_num[index] !== undefined && list1_num[index] !== null) {
+               let temp = (parseInt(list1_num[index], 10) + parseInt(list1_num[index], 10));
+                if (temp < 10) {
+                    total.push(temp.toString());
+                    if (lastOver10) {
+                        lastOver10 = false;
+                        total[index] = (parseInt(total[index]) + 1).toString();
+                    }
+                }
+                else {
+                    if (lastOver10) {
+                        lastOver10 = false;
+                        total.push('1')
+                    }
+                    lastOver10 = true;
+                    total.push(temp.toString().substring(1));
+                }
+            } else {
+                total.push(list2_num[index]);
+            }
+        }
+    }
+    total.shift();
+    const sum = total;
+    const nums = sum.map((item) => (item = parseInt(item, 10)));
+    console.log('nums', nums)
     const next_nums = [...nums];
+    next_nums.shift();
     return_list = constructList(nums[0], next_nums);
     return return_list
 };
